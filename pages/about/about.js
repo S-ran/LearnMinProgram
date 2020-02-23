866d5aacac0cdac00e741d655fab344b9a3f8c28
S-ran <2670070318@qq.com>
2020-02-23 19:21:32 +0800

    小程序登录

--- pages/about/about.js
+++ pages/about/about.js
@@ -1,66 +1,62 @@
 // pages/about/about.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  globalData:{
+    token:''
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    //先从缓存中取出token
+    const token = wx.getStorageSync('token')
+    //判断token是否有值
+    if(token&&token.length!==0){
+      //已经有token了验证token是否过期
+      this.check(token)
+    }else{this.logo()}
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  //验证操作
+  check(token){
+    wx.request({
+      url: 'http://123.207.32.32:3000/auth',
+      method:'post',
+      header:{
+        token
+      },
+      success(res){
+        if(!res.data.errCode){
+          this.globalData.token = token;
+        }
+      },
+      fail(err) {
+        this.logo()
+      }
+    })
   },
+    //登陆操作
+    logo(){
+      wx.login({
+        success:(res)=>{
+          console.log(res)
+          //获取code
+          const code = res.code;
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+          //2.将code发送给服务器
+          wx.request({
+            url: 'http://123.207.32.32:3000/login',
+            data:{
+              code
+            },
+            success:(res) =>{
+              console.log(res)
+              //1.取出token
+              const token = res.data.token;
+              //2.将token保存在globalData中
+              this.globalData.token = token;
+              //3.进行本地存储
+              wx.setStorageSync('token', token)
+        
+            }
+          })
+        }
+      })
+    }
+  
 })
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,11 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onShareAppMessage: function (options) {
+    return{
+      title:'编号ua',
+      path:'pages/about/about',
+      imageUrl:'http://img3.imgtn.bdimg.com/it/u=583533740,1632379423&fm=11&gp=0.jpg',
+      
+    }
   }
 })
